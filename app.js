dce056cb5c78fdfef01af0af8a7365cef18f74ea
sainttobs <sainttobs@gmail.com>
2019-04-08 15:07:30 +0100

    logging details

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+console.log("Server running on PORT 3000");
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
