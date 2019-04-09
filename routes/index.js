4cdf93093f70332e25fe5b983f1496e06ef15835
Abubakar <talk2asquare@gmail.com>
2019-04-09 11:01:05 +0100

    Modified the controllers, routes and added a promise handler for the controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var Jobs = require("../controllers/controllers");
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Remote Job Alert' });
 });
+
+router.get('/remote-jobs', Jobs.get_all)
+router.post('/remote-jobs', Jobs.create);
+router.get('/remote-jobs/:job_id', Jobs.get_one);
+router.get('/remote-jobs/:job_id', Jobs.edit);
+router.get('/remote-jobs/:job_id', Jobs.update_job);
+router.get('/remote-jobs/:job_id', Jobs.cancel_job);
 
 module.exports = router;
