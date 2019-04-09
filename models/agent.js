1fdb4e428671ef7155869c8afb31f0d45a878eb7
Joy Joel <joyjoel2011@gmail.com>
2019-04-09 21:06:43 +0100

    added agent model only

---
+++ models/agent.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongooose');
+var Schema = mongoose.Schema;
+
+const agentSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: Number,
+        required: true
+    },
+    job_role: {
+        type: String,
+        required: true
+    },
+});
+
+
+module.exports = mongoose.model('Agent', agentSchema);
