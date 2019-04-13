38be4f4083eab105a8ab268318d6e5340ebb7266
Abubakar <talk2asquare@gmail.com>
2019-04-13 12:08:03 +0100

    Redirect Hire and Agent button to Form to fill with email and phone number for individual job view

--- models/jobs.js
+++ models/jobs.js
@@ -15,10 +15,6 @@ const jobSchema = new Schema(
       type: String,
       required: false
     },
-    employer_link: {
-      type: String,
-      required: false
-    },
     job_description: {
       type: String,
       required: true
