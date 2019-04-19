114e5db299d2ce4334885402648d66dc8847ff0f
Somtozech <ezechinnaemeka@gmail.com>
2019-04-19 17:39:44 +0100

    Changed email schedule to 6pm on friday

--- schedule.js
+++ schedule.js
@@ -2,7 +2,7 @@ const UserController = require('./controllers/user');
 const schedule = require('node-schedule');
 
 var j = schedule.scheduleJob(
-  { hour: 14, minute: 10, dayOfWeek: 4 },
+  { hour: 18, minute: 30, dayOfWeek: 5 },
   UserController.sendMailForRemoteJob
 );
 
