9d6047436b91e36f200d3195a9ac360638f3987d
IDTitanium <idriseun222@gmail.com>
2019-05-02 23:59:05 +0100

    Backend Completion of Featured Jobs

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,6 +4,7 @@ const fetch = require("node-fetch");
 const { sendMailForRemoteJob } = require("./user");
 const userModel = require("../models/user");
 
+
 const Jobs = {
   async fetchData(req, res) {
     let data = await fetch("https://jobs.github.com/positions.json?location=remote");
@@ -62,7 +63,12 @@ const Jobs = {
           inc: function(index) {
             index++;
             return index;
-          }
+          },
+          limit: function (arr, limit) {
+          if (!Array.isArray(arr)) { return []; }
+            return arr.slice(0, limit);
+        }
+
         }
       });
     } catch (error) {
@@ -84,7 +90,7 @@ const Jobs = {
     };
     try {
       let foundJob = await db.findOne(queryText);
-      return res.status(200).render("/", { content: foundJob });
+      return res.status(200).render("job_info_page_dummy", { content: foundJob });
     } catch (error) {
       return res.status(400).send(error);
     }
