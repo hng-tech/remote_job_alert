b4abf82052b2007c767e1a83d3cb5b3af9d4fdd8
Abdullahi Ayobami Oladosu <ayobami.oladosu@gmail.com>
2019-05-17 12:13:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.use(function(req, res, next) {
 });
 
 app.use("/", indexRouter);
-// app.use("/auth.js", authRouter);
+app.use("/auth", authRouter);
 // app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
